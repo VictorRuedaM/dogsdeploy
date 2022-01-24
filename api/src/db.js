7149112a3c49af2e5396c7dd6057b7addad061da
Victor Rueda <vialruma1985@gmail.com>
2022-01-24 14:54:32 -0500

    dogs

--- api/src/db.js
+++ api/src/db.js
@@ -32,7 +32,7 @@ let sequelize =
         },
         ssl: true,
       })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`, {
+    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
